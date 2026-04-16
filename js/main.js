4083f0825cd9fb12653ed6bd01a9a44e12020f8e
naresh4727 <109799023+naresh4727@users.noreply.github.com>
2026-04-16 13:04:44 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(function() {
+	// Initialize products slider (Slick)
+	if ($.fn.slick) {
+		$('.products-slider').slick({
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			dots: true,
+			arrows: true,
+			infinite: true,
+			autoplay: false,
+			// Disable adaptiveHeight so all slides can share a uniform height
+			adaptiveHeight: false,
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						slidesToShow: 2
+					}
+				},
+				{
+					breakpoint: 576,
+					settings: {
+						slidesToShow: 1
+					}
+				}
+			]
+		});
+
+		// Ensure all slides have the same height
+		function setEqualSlideHeights() {
+			var $cards = $('.products-slider .product-card');
+			// reset
+			$cards.css('height', 'auto');
+			var max = 0;
+			$cards.each(function() {
+				var h = $(this).outerHeight();
+				if (h > max) max = h;
+			});
+			if (max > 0) {
+				$cards.css('height', max + 'px');
+				$('.products-slider .slick-slide').css('height', max + 'px');
+			}
+		}
+
+		// Run on init and after layout changes
+		$('.products-slider').on('init reInit setPosition', function() {
+			setEqualSlideHeights();
+		});
+
+		// Debounced resize
+		var resizeTimer;
+		$(window).on('resize', function() {
+			clearTimeout(resizeTimer);
+			resizeTimer = setTimeout(function() {
+				setEqualSlideHeights();
+				$('.products-slider').slick('setPosition');
+			}, 150);
+		});
+	}
+});
